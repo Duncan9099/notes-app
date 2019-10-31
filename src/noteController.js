af0800c98163d752afbc9de39389ad74d8421050
Duncan Skinner <duncanskinner94@gmail.com>
2019-10-31 15:37:13 +0000

    prevent default stops page refresh

--- src/noteController.js
+++ src/noteController.js
@@ -3,32 +3,41 @@
   function NoteController(noteListView) {
     this.noteListView = noteListView
     this.displayHTML()
+
+    makeUrlChangeShowNoteForCurrentPage();
+
+    addNote();
+
+    function makeUrlChangeShowNoteForCurrentPage() {
+      window.addEventListener("hashchange", showNoteForCurrentPage);
+    };
+
+    function showNoteForCurrentPage() {
+      showNote(getNoteFromUrl(window.location));
+    };
+
+    function getNoteFromUrl(location) {
+      return location.hash.split("#notes/")[1];
+    };
+
+    function showNote(noteID) {
+      console.log(this)
+      document
+        .getElementById("app")
+        .innerHTML = this.noteListView.getById(noteID).text
+    };
+
+    function addNote() {
+      document.getElementById("text").addEventListener("submit", function(submit) {
+        submit.preventDefault();
+        console.log(submit)
+      })
+    }
   }
 
   NoteController.prototype.displayHTML = function(doc = document) {
     return doc.getElementById("app").innerHTML = this.noteListView.getHTML()
   }
-
-  makeUrlChangeShowNoteForCurrentPage();
-
-  function makeUrlChangeShowNoteForCurrentPage() {
-    window.addEventListener("hashchange", showNoteForCurrentPage);
-  };
-
-  function showNoteForCurrentPage() {
-    showNote(getNoteFromUrl(window.location));
-  };
-
-  function getNoteFromUrl(location) {
-    return location.hash.split("#notes/")[1];
-  };
-
-  function showNote(noteID) {
-    console.log(this)
-    document
-      .getElementById("app")
-      .innerHTML = this.noteListView.getById(noteID).text
-  };
 
   exports.NoteController = NoteController;
 })(this)
