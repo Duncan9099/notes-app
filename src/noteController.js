98bde5bf76d4be5d1993e6881d03468d24c302c0
Duncan Skinner <duncanskinner94@gmail.com>
2019-10-31 15:17:36 +0000

    directed to new notes page

--- src/noteController.js
+++ src/noteController.js
@@ -1,9 +1,6 @@
 
 (function(exports) {
   function NoteController(noteListView) {
-    console.log('notes list');
-    // this.noteList = noteList
-    // this.note = this.noteList.addNote("Favourite drink: seltzer")
     this.noteListView = noteListView
     this.displayHTML()
   }
@@ -11,47 +8,6 @@
   NoteController.prototype.displayHTML = function(doc = document) {
     return doc.getElementById("app").innerHTML = this.noteListView.getHTML()
   }
-
-  // NoteController.prototype.makeUrlChangeShowNoteForCurrentPage = function() {
-  //   window.addEventListener("hashchange", showNoteForCurrentPage)
-  //
-  //   function showNoteForCurrentPage() {
-  //     showNote(getNoteFromUrl())
-  //   }
-  //
-  //   function getNoteFromUrl() {
-  //     console.log(window.location.href.split("#")[1])
-  //     note = this.noteListView.getById(window.location.href.split("#")[1])
-  //     return note.getText()
-  //   }
-  //
-  //   function showNote(note) {
-  //     document.getElementsbyId("note").innerHTML = note
-  //   }
-  // }
-  //
-  // noteController.makeUrlChangeShowNoteForCurrentPage();
-
-  // NoteController.prototype.makeUrlChangeShowNoteForCurrentPage = function() {
-  //   return window.addEventListener("hashchange", this.showNoteForCurrentPage)
-  // }
-  //
-  // NoteController.prototype.showNoteForCurrentPage = function() {
-  //   showNote(getNoteFromUrl())
-  // }
-  //
-  // NoteController.prototype.getNoteFromUrl = function() {
-  //   var id = window.location.href.split("#")[1]
-  //   console.log(id)
-  //   var note = this.noteListView.getById(id)
-  //   console.log(note)
-  //   console.log(this.noteListView)
-  //   console.log(this.noteListView.getById(window.location.href.split("#notes/")[1]))
-  // }
-  //
-  // NoteController.prototype.showNote = function(noteID) {
-  //   document.getElementsbyId("note").innerHTML = note.getText()
-  // }
 
   makeUrlChangeShowNoteForCurrentPage();
 
@@ -68,7 +24,7 @@
   };
 
   function showNote(noteID) {
-    console.log(noteID)
+    console.log(this)
     document
       .getElementById("app")
       .innerHTML = this.noteListView.getById(noteID).text
